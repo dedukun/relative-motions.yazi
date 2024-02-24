ff6e516fb8e376044b297a2f4c4a3d990a6a236c
sxyazi <sxyazi@gmail.com>
2024-02-25 04:20:34 +0800

    Add StyLua configuration and format the code

--- init.lua
+++ init.lua
@@ -1,24 +1,24 @@
 -- stylua: ignore
 local MOTIONS_AND_OP_KEYS = {
-	{ on = "0"}, { on = "1"}, { on = "2"}, { on = "3"}, { on = "4"},
-	{ on = "5"}, { on = "6"}, { on = "7"}, { on = "8"}, { on = "9"},
+	{ on = "0" }, { on = "1" }, { on = "2" }, { on = "3" }, { on = "4" },
+	{ on = "5" }, { on = "6" }, { on = "7" }, { on = "8" }, { on = "9" },
 	-- commands
-	{ on = "d"}, { on = "v"}, { on = "y"}, { on = "x"},
+	{ on = "d" }, { on = "v" }, { on = "y" }, { on = "x" },
 	-- movement
-	{ on = "g"}, { on = "j"}, { on = "k"}
+	{ on = "g" }, { on = "j" }, { on = "k" }
 }
 
 -- stylua: ignore
 local MOTION_KEYS = {
-	{ on = "0"}, { on = "1"}, { on = "2"}, { on = "3"}, { on = "4"},
-	{ on = "5"}, { on = "6"}, { on = "7"}, { on = "8"}, { on = "9"},
+	{ on = "0" }, { on = "1" }, { on = "2" }, { on = "3" }, { on = "4" },
+	{ on = "5" }, { on = "6" }, { on = "7" }, { on = "8" }, { on = "9" },
 	-- movement
-	{ on = "g"}, { on = "j"}, { on = "k"}
+	{ on = "g" }, { on = "j" }, { on = "k" }
 }
 
 -- stylua: ignore
 local DIRECTION_KEYS = {
-	{ on = "j"}, { on = "k"}
+	{ on = "j" }, { on = "k" }
 }
 
 local SHOW_NUMBERS_ABSOLUTE = 0
@@ -32,15 +32,13 @@ local SHOW_NUMBERS_RELATIVE_ABSOLUTE = 2
 local render_motion_setup = ya.sync(function()
 	ya.render()
 
-	Status.motion = function()
-		return ui.Span("")
-	end
+	Status.motion = function() return ui.Span("") end
 
 	Status.render = function(self, area)
 		self.area = area
 
-		local left = ui.Line({ self:mode(), self:size(), self:name() })
-		local right = ui.Line({ self:motion(), self:permissions(), self:percentage(), self:position() })
+		local left = ui.Line { self:mode(), self:size(), self:name() }
+		local right = ui.Line { self:motion(), self:permissions(), self:percentage(), self:position() }
 		return {
 			ui.Paragraph(area, { left }),
 			ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
@@ -66,16 +64,16 @@ local render_motion = ya.sync(function(_, motion_num, motion_cmd)
 			motion_span = ui.Span(string.format(" %3d%s ", motion_num, motion_cmd)):style(style)
 		end
 
-		return ui.Line({
+		return ui.Line {
 			ui.Span(THEME.status.separator_open):fg(style.bg),
 			motion_span,
 			ui.Span(THEME.status.separator_close):fg(style.bg),
 			ui.Span(" "),
-		})
+		}
 	end
 end)
 
-local render_numbers = ya.sync(function(state, mode)
+local render_numbers = ya.sync(function(_, mode)
 	ya.render()
 
 	File.number = function(_, index, file, hovered)
@@ -119,7 +117,7 @@ local render_numbers = ya.sync(function(state, mode)
 
 		local items, markers = {}, {}
 		for i, f in ipairs(files) do
-			items[#items + 1] = ui.ListItem(ui.Line(ya.flat({ File:number(i, f, hovered_index), File:full(f) })))
+			items[#items + 1] = ui.ListItem(ui.Line(ya.flat { File:number(i, f, hovered_index), File:full(f) }))
 				:style(File:style(f))
 
 			-- Yanked/marked/selected files
@@ -129,25 +127,21 @@ local render_numbers = ya.sync(function(state, mode)
 			end
 		end
 
-		return ya.flat({
+		return ya.flat {
 			ui.List(area, items),
 			Folder:linemode(area, files),
 			Folder:markers(area, markers),
-		})
+		}
 	end
 end)
 
-local function render_clear()
-	render_motion()
-end
+local function render_clear() render_motion() end
 
 -----------------------------------------------
 --------- C O M M A N D   P A R S E R ---------
 -----------------------------------------------
 
-local get_keys = ya.sync(function(state)
-	return state._only_motions and MOTION_KEYS or MOTIONS_AND_OP_KEYS
-end)
+local get_keys = ya.sync(function(state) return state._only_motions and MOTION_KEYS or MOTIONS_AND_OP_KEYS end)
 
 local function get_cmd(first_char, keys)
 	local last_key
@@ -155,7 +149,7 @@ local function get_cmd(first_char, keys)
 
 	while true do
 		render_motion(tonumber(lines))
-		local key = ya.which({ cands = keys, silent = true })
+		local key = ya.which { cands = keys, silent = true }
 		if not key then
 			return nil, nil, nil
 		end
@@ -176,7 +170,7 @@ local function get_cmd(first_char, keys)
 		DIRECTION_KEYS[#DIRECTION_KEYS + 1] = {
 			on = last_key,
 		}
-		local direction_key = ya.which({ cands = DIRECTION_KEYS, silent = true })
+		local direction_key = ya.which { cands = DIRECTION_KEYS, silent = true }
 		if not direction_key then
 			return nil, nil, nil
 		end
